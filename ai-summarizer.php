d4f0499eae1326cb23f7f4728f37e1290e46ffc6
theaminuldev <mdaminulislam2015bd@gmail.com>
2026-01-19 18:20:50 +0600

    fix

--- ai-summarizer.php
+++ ai-summarizer.php
@@ -5,8 +5,6 @@
  * Version:           0.4.0
  * Requires at least: 6.1
  * Requires PHP:      7.4
- * Author:            theaminul
- * Author URI:        https://theaminul.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       ai-summarizer
